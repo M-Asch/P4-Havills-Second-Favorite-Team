4810dfd59b0b68326ec4723464a748eaa59e9b3d
Riley Collins <collin_r2@denison.edu>
2022-04-07 18:36:27 -0400

    foundation

--- receiver.h
+++ receiver.h
@@ -1,1 +1,43 @@
 // receiver.h
+
+#include <iostream>
+
+#include <stdio.h> //printf
+#include <stdlib.h> //exit
+#include <unistd.h> //close
+#include <string.h> //memset
+#include <arpa/inet.h> //inet_ntop
+#include <netdb.h> //sender
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <poll.h>
+
+
+//===========================
+//		  Definitions
+//
+//===========================
+
+#define SERVERPORT 9090
+#define SERVERPORTS "9090"
+#define MAXBUFLEN 100
+
+using namespace std;
+
+class Receiver{
+
+  public:
+    Receiver();
+
+    int initialReceive();
+    int waitForMessage();
+    int sendAck();
+
+    void receiveMessage(int argc, char **argv);
+
+  private:
+    int lastRecieve;
+    int lastAcceptable;
+};
