4dc2d6d5bcaadf36f61ea67be02c6af7aca5b0bd
Mitch <ASchme_m1@denison.edu>
2022-04-07 18:19:00 -0400

    basic class setup

--- sender.cpp
+++ sender.cpp
@@ -3,11 +3,61 @@
 
 using namespace std;
 
-Sender::Sender(){
-	message = [1024];
-	length = 1024;
+Sender::Sender(int size, char *msg){
+	message = msg;
+	length = size;
 	lastSent = 0;
 	lastAck = 0;
+}
+
+void Sender::setMessage(int size, char *msg){
+	message = msg;
+	length = size;
+}
+
+int Sender::slidingWindow(){
+	return 0;
+}
+
+int Sender::initialMessage(){
+	return 0;
+}
+
+void Sender::sendMessage(char* buffer, char* sender_ip, char* p){
+
+	struct addrinfo hints, *server_info;
+
+ 	memset(&hints, 0, sizeof hints);
+ 	hints.ai_family = AF_INET; //IPv4
+ 	hints.ai_socktype = SOCK_DGRAM; // UDP socket
+
+ 	int status = getaddrinfo(sender_ip, p, &hints, &server_info);
+ 	if (status != 0)
+ 	{
+ 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+ 		exit(1);
+ 	}
+
+ 	int newSock;	//newsocket to send info to
+ 	struct addrinfo *ptr = server_info; //ptr to current struct addrinfo
+ 	while (ptr != NULL){				//attempt to build a socket for the senders ip and port
+ 		newSock = socket(ptr->ai_family, ptr->ai_socktype, 0);
+ 		if (newSock != -1){
+ 			break;
+ 		}
+ 		ptr = ptr->ai_next;
+ 	}
+ 	if (ptr == NULL){
+ 		fprintf(stderr, "client: failed to create socket\n");
+ 		exit(2);
+ 	}
+
+ 	int numbytes = sendto(newSock, buffer, MAXBUFLEN, 0, ptr->ai_addr, ptr->ai_addrlen);		//send out last message
+ 	if ((numbytes) == -1)
+ 	{
+ 		perror("client: sendto");
+ 		exit(1);
+ 	}
 }
 
 int main (void){
--- sender.h
+++ sender.h
@@ -1,22 +1,41 @@
 #include <iostream>
 
+#include <stdio.h> //printf
+#include <stdlib.h> //exit
+#include <unistd.h> //close
+#include <string.h> //memset
+#include <arpa/inet.h> //inet_ntop
+#include <netdb.h> //sender
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <poll.h>
+
+//===========================
+//		  Definitions 
+//
+//===========================
+
+#define SERVERPORT 9090
+#define SERVERPORTS "9090"
+#define MAXBUFLEN 100
+
 using namespace std;
 
 class Sender{
 
 	public:
-		Sender();
-		Sender(int size);
 		Sender(int size, char *msg);
 	
 		void setMessage(int size, char *msg);
 	
 		int slidingWindow();
 		int initialMessage();
-		int sendMessage();
+		void sendMessage(char* buffer, char* sender_ip, char* p);
 	
 	private:
-		char *message;
+		char* message;
 		int length;
 		int lastSent;
 		int lastAck;
